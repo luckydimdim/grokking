08ae38037393d7ea0f8a72972f82836807495352
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-05-25 16:50:28 +0400

    Cyclic Sort

---
+++ cyclic_sort/cyclic_sort/main.py
@@ -0,0 +1,27 @@
+def cyclic_sort(nums):
+    '''
+    We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number
+    from 1 to ‘n’ based on their creation sequence. This means that the object with sequence number ‘3’
+    was created just before the object with sequence number ‘4’.
+
+    Write a function to sort the objects in-place on their creation sequence number in O(n)
+    and without any extra space. For simplicity, let’s assume we are passed an integer array
+    containing only the sequence numbers, though each number is actually an object.
+    '''
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+
+        #if nums[i] == i + 1:
+        if nums[i] == i + 1:
+            i += 1
+        else:
+            nums[i], nums[j] = nums[j], nums[i]
+
+    return nums
+
+# [1, 2, 3, 4, 5]
+
+nums = [3, 1, 5, 4, 2]
+
+print(cyclic_sort(nums))
