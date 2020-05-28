62a2620759b32d8c5356e5b025b91b21eafd3957
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-05-28 20:48:03 +0400

    Connect All Level Order Siblings

--- tree_breadth_first_search/connect_all_level_order_siblings/main.py
+++ tree_breadth_first_search/connect_all_level_order_siblings/main.py
@@ -7,27 +7,19 @@ class TreeNode:
     self.val = val
     self.left, self.right, self.next = None, None, None
 
-  # level order traversal using 'next' pointer
-  def print_level_order(self):
-    nextLevelRoot = self
-    while nextLevelRoot:
-      current = nextLevelRoot
-      nextLevelRoot = None
-      while current:
-        print(str(current.val) + " ", end='')
-        if not nextLevelRoot:
-          if current.left:
-            nextLevelRoot = current.left
-          elif current.right:
-            nextLevelRoot = current.right
-        current = current.next
-      print()
+  # tree traversal using 'next' pointer
+  def print_tree(self):
+    print("Traversal using 'next' pointer: ", end='')
+    current = self
+    while current:
+      print(str(current.val) + " ", end='')
+      current = current.next
 
 
-def connect_level_order_siblings(root):
+def connect_all_siblings(root):
   '''
   Given a binary tree, connect each node with its level order successor.
-  The last node of each level should point to a null node.
+  The last node of each level should point to the first node of the next level.
   '''
   if root is None:
     return root
@@ -35,9 +27,10 @@ def connect_level_order_siblings(root):
   queue = deque()
   queue.append(root)
 
+  prev = None
+
   while queue:
     level_size = len(queue)
-    prev = None
 
     for _ in range(level_size):
       curr = queue.popleft()
@@ -62,10 +55,8 @@ def main():
   root.left.left = TreeNode(9)
   root.right.left = TreeNode(10)
   root.right.right = TreeNode(5)
-  connect_level_order_siblings(root)
-
-  print("Level order traversal using 'next' pointer: ")
-  root.print_level_order()
+  connect_all_siblings(root)
+  root.print_tree()
 
 
 main()
