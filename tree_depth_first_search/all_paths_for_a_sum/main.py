a748fa070b9fd51f789903d29146f39f822d82bc
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-05-29 15:32:09 +0400

    All Paths for a Sum

---
+++ tree_depth_first_search/all_paths_for_a_sum/main.py
@@ -0,0 +1,48 @@
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def find_paths(root, sum):
+  '''
+  Given a binary tree and a number ‘S’,
+  find all paths from root-to-leaf
+  such that the sum of all the node values of each path equals ‘S’.
+  '''
+  allPaths = []
+
+  find_paths_recursive(root, sum, [], allPaths)
+
+  return allPaths
+
+def find_paths_recursive(root, sum, current, result):
+  if root is None:
+    return
+
+  current.append(root.val)
+
+  if sum == root.val and root.left is None and root.right is None:
+    result.append(list(current))
+
+  find_paths_recursive(root.left, sum - root.val, current, result)
+  find_paths_recursive(root.right, sum - root.val, current, result)
+
+  del current[-1]
+
+
+def main():
+
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(4)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  sum = 23
+  print("Tree paths with sum " + str(sum) +
+        ": " + str(find_paths(root, sum)))
+
+
+main()
