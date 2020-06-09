864c7cfe680759fbabc0a485eb04be0147cbdb95
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-06-09 08:38:33 +0400

    Evaluate Expression

--- subsets/evaluate_expression/main.py
+++ subsets/evaluate_expression/main.py
@@ -6,7 +6,25 @@ def diff_ways_to_evaluate_expression(input):
     '''
     result = []
 
+    if '+' not in input and '-' not in input and '*' not in input:
+        result.append(int(input))
+    else:
+        for i in range(len(input)):
+            curr = input[i]
+            if not curr.isdigit():
+                left_part = diff_ways_to_evaluate_expression(input[0:i])
+                right_part = diff_ways_to_evaluate_expression(input[i+1:])
+
+                for part1 in left_part:
+                    for part2 in right_part:
+                        if curr == '+':
+                            result.append(part1 + part2)
+                        elif curr == '-':
+                            result.append(part1 - part2)
+                        elif curr == '*':
+                            result.append(part1 * part2)
     return result
+
 
 def main():
     print("Expression evaluations: " +
@@ -17,3 +35,9 @@ def main():
 
 
 main()
+
+# 1+2*3
+# (1+2)*3 (1+2*3) (1)+(2)*(3) 1+(2*3)
+#
+# 2*3-4-5
+# ((2*3)-4)-5 2*((3-4)-5)
