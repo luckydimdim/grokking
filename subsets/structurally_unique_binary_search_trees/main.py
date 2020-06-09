243b91cb891b905fda078ddf6636b05bacf27184
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-06-09 09:35:26 +0400

    Structurally Unique Binary Search Trees

---
+++ subsets/structurally_unique_binary_search_trees/main.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def find_unique_trees2(n):
+    '''
+    Given a number ‘n’, write a function to
+    return all structurally unique Binary Search Trees (BST)
+    that can store values 1 to ‘n’?
+    '''
+    if n <= 0:
+        return []
+
+    return find_unique_trees_recursive(1, n)
+
+def find_unique_trees_recursive(start, end):
+    result = []
+    if start > end:
+        result.append(None)
+        return result
+
+    for i in range(start, end + 1):
+        left_tree = find_unique_trees_recursive(start, i - 1)
+        right_tree = find_unique_trees_recursive(i + 1, end)
+        for left in left_tree:
+            for right in right_tree:
+                root = TreeNode(i)
+                root.left = left
+                root.right = right
+                result.append(root)
+
+    return result
+
+def main():
+    print("Total trees: " + str(len(find_unique_trees(2))))
+    print("Total trees: " + str(len(find_unique_trees(3))))
+
+
+main()
