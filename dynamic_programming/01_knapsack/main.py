0b38cda1f56184ef1db8d8c51394d571b5a1d6b5
Dmitriy Litovchenko <msk.roytop@gmail.com>
2020-06-12 15:20:36 +0400

    0/1 Knapsack

---
+++ dynamic_programming/01_knapsack/main.py
@@ -0,0 +1,93 @@
+def solve_knapsack2(profits, weights, capacity):
+  memo = [[-1 for x in range(capacity+1)] for y in range(len(profits))]
+  return knapsack_recursive(memo, profits, weights, capacity, 0)
+
+def knapsack_recursive(memo, profits, weights, capacity, index):
+  if index >= len(profits) or capacity <= 0:
+    return 0
+
+  if memo[index][capacity] != -1:
+    return memo[index][capacity]
+
+  left_profit, right_profit = 0, 0
+  if weights[index] <= capacity:
+    left_profit = profits[index] + knapsack_recursive(memo, profits, weights, capacity - weights[index], index+1)
+
+  right_profit = knapsack_recursive(memo, profits, weights, capacity, index+1)
+
+  max_profit = max(left_profit, right_profit)
+
+  if memo[index][capacity] == -1:
+    memo[index][capacity] = max_profit
+
+  return max_profit
+
+def solve_knapsack3(profits, weights, capacity):
+  n = len(profits)
+
+  if capacity <= 0 or n == 0 or len(weights) != n:
+    return 0
+
+  dp = [[0 for x in range(capacity+1)] for y in range(2)]
+
+  for c in range(capacity+1):
+    if weights[0] <= c:
+      dp[0][c] = dp[1][c] = profits[0]
+
+  for i in range(1, n):
+    for c in range(1, capacity+1):
+      profit1, profit2 = 0, 0
+      if weights[i] <= c:
+        profit1 = profits[i] + dp[(i - 1) % 2][c - weights[i]]
+
+      profit2 = dp[(i - 1) % 2][c]
+      dp[i % 2][c] = max(profit1, profit2)
+
+  print_selected_elements(dp, weights, profits, capacity)
+
+  return dp[(n - 1) % 2][capacity]
+
+def print_selected_elements(dp, weights, profits, capacity):
+  result = []
+  n = len(weights)
+  total = dp[(n - 1) % 2][capacity]
+  for i in range(n-1, 0, -1):
+    if total != dp[(i - 1) % 2][capacity]:
+      result.append(weights[i])
+      capacity -= weights[i]
+      total -= profits[i]
+
+  if total != 0:
+    result.append(weights[0])
+
+  print(result)
+
+def solve_knapsack(profits, weights, capacity):
+  n = len(profits)
+
+  if capacity <= 0 or n == 0 or len(weights) != n:
+    return 0
+
+  dp = [0 for x in range(capacity+1)]
+
+  for c in range(capacity+1):
+    if weights[0] <= c:
+      dp[c] = profits[0]
+
+  for i in range(1, n):
+    for c in range(capacity, -1, -1):
+      profit1, profit2 = 0, 0
+      if weights[i] <= c:
+        profit1 = profits[i] + dp[c - weights[i]]
+
+      profit2 = dp[c]
+      dp[c] = max(profit1, profit2)
+
+  return dp[capacity]
+
+def main():
+  print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 5))
+  print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 6))
+  print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
+
+main()
